f3ec90a8616fa47233d171fa3a0c842968a6fbb0
Eli Ingram <ingrame25@hanover.edu>
2023-11-02 14:31:54 -0400

    Created the instructor class

---
+++ src/main/java/edu/hanover/schedulevisualizer/core/Instructor.java
@@ -0,0 +1,11 @@
+package edu.hanover.schedulevisualizer.core;
+
+public class Instructor {
+    private final String first;
+    private final String last;
+
+    public Instructor(String first, String last, int Id) {
+        this.first = first;
+        this.last = last;
+    }
+}
---
+++ src/test/java/edu/hanover/schedulevisualizer/core/InstructorTest.java
@@ -0,0 +1,10 @@
+package edu.hanover.schedulevisualizer.core;
+
+import org.junit.Test;
+
+public class InstructorTest {
+    @Test
+    public void instructorsHaveFirstAndLastName(){
+        Instructor instructor = new Instructor(first, last);
+    }
+}
