b9dbf8ae69ab69da610b560a6df478d55e481778
Jayson Baya <yesjaysonb@gmail.com>
2023-11-07 15:56:39 -0500

    checkForInstructorTimeSlotOverlap change test description

--- src/test/java/edu/hanover/schedulevisualizer/core/InstructorTest.java
+++ src/test/java/edu/hanover/schedulevisualizer/core/InstructorTest.java
@@ -19,7 +19,7 @@ public class InstructorTest {
 
     @Test
     public void checkForInstructorTimeSlotOverlapTrue() {
-        // Create an instructor with two sections at the same time
+        // Create a section with same TimeSlot and same Instructors
         Instructor instructor = new Instructor("Barbara", "Wahl", "wahl@hanover.edu");
         Section section1 = new Section("CS", "220", "Fundamentals of Computer Science", Context.getInstance().makeHCTimeSlot(Weekday.MWF(), 1));
         Section section2 = new Section("MAT", "121", "Calculus I", Context.getInstance().makeHCTimeSlot(Weekday.MWF(), 1));
@@ -33,7 +33,7 @@ public class InstructorTest {
 
     @Test
     public void checkForInstructorTimeSlotOverlapFalse() {
-        // Create an instructor with two sections at different times
+        // Create a section with same TimeSlot and different Instructors
         Instructor instructor = new Instructor("Barbara", "Wahl", "wahl@hanover.edu");
         Section section1 = new Section("CS", "220", "Fundamentals of Computer Science", Context.getInstance().makeHCTimeSlot(Weekday.MWF(), 1));
         Section section2 = new Section("MAT", "121", "Calculus I", Context.getInstance().makeHCTimeSlot(List.of(Weekday.Tuesday), 7));
