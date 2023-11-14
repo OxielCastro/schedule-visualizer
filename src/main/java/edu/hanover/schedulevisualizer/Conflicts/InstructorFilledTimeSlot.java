2f89cfdc68bbbe998ee64db412d6eb2d301435c2
DavidSnowden <snowdend@hanover.edu>
2023-11-14 15:54:26 -0500

    Made Conflict for two instructors being in the same timeslot. Tested for having two instructors in the same time slot and two instructors not in the same time slot.

---
+++ src/main/java/edu/hanover/schedulevisualizer/Conflicts/InstructorFilledTimeSlot.java
@@ -0,0 +1,16 @@
+package edu.hanover.schedulevisualizer.Conflicts;
+
+import edu.hanover.schedulevisualizer.core.Instructor;
+import edu.hanover.schedulevisualizer.core.Section;
+
+public class InstructorFilledTimeSlot implements Conflict {
+    Instructor instructor;
+
+    public boolean IsSameInstructor(Section section1, Section section2) {
+        if(section1.getInstructorList().equals(section2.getInstructorList())) {
+            return section1.getTimeslot().overlaps(section2.getTimeslot());
+        }
+        return false;
+    }
+
+}
