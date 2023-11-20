7ae90e877199ea5e268bfeb9218c695d8357b5f2
Matt Burchett <burchettm26@hanover.edu>
2023-11-20 15:35:21 -0500

    Added toString method for the InstructorConflict class

--- src/main/java/edu/hanover/schedulevisualizer/Conflicts/InstructorConflict.java
+++ src/main/java/edu/hanover/schedulevisualizer/Conflicts/InstructorConflict.java
@@ -14,6 +14,11 @@ public class InstructorConflict implements Conflict {
     }
 
     @Override
+    public String toString() {
+        return "InstructorConflict";
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
