3a9e8829ab6493899bf8fcc5bde2ddda142b22b4
Haris Skiadas <cskiadas@gmail.com>
2024-01-23 13:10:48 -0500

    Add toolchain detection

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,16 @@
  */
 
 rootProject.name = "schedule-visualizer"
+plugins {
+    id("org.gradle.toolchains.foojay-resolver") version "0.8.0"
+}
+
+toolchainManagement {
+    jvm {
+        javaRepositories {
+            repository("foojay") {
+                resolverClass.set(org.gradle.toolchains.foojay.FoojayToolchainResolver::class.java)
+            }
+        }
+    }
+}
