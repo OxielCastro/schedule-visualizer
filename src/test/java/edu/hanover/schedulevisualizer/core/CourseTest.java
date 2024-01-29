e0ddd5a3a35c6a095cb95a0228e22471bc169ec1
Haris Skiadas <cskiadas@gmail.com>
2024-01-29 15:30:23 -0500

    Fix back the error

--- src/test/java/edu/hanover/schedulevisualizer/core/CourseTest.java
+++ src/test/java/edu/hanover/schedulevisualizer/core/CourseTest.java
@@ -10,7 +10,7 @@ public class CourseTest {
     @Test
     public void equalsMethodWorks() {
         Course course1 = new Course("CS", "321", "Software Development Practicum");
-        Course course2 = new Course("C", "321", "Software Development Practicum");
+        Course course2 = new Course("CS", "321", "Software Development Practicum");
         assertTrue(course1.equals(course2));
         Course course3 = new Course("CS", "223", "Data Structures");
         assertFalse(course1.equals(course3));
