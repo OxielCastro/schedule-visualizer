e6261a74f8347e423da51a5028d1a5e566a45469
Jayson Baya <yesjaysonb@gmail.com>
2024-02-06 13:17:36 -0500

    Update Constraint.java -- Java Doc, error fixed

--- src/main/java/edu/hanover/schedulevisualizer/conflict/Constraint.java
+++ src/main/java/edu/hanover/schedulevisualizer/conflict/Constraint.java
@@ -16,7 +16,7 @@ public interface Constraint {
     /**
      * Generates a list of conflicts based on the given list of course sections.
      * Each pair of sections in the list is checked for conflicts, and a list of optional conflicts is returned.
-     * @param `sectionList` the list of course sections to generate conflicts for
+     * @param sectionlist the list of course sections to generate conflicts for
      * @return a list of optional conflicts between course sections
      */
     default List<Optional<Conflict>> generateConflicts(final List<Section> sectionlist) {
@@ -32,8 +32,8 @@ public interface Constraint {
     /**
      * Checks if two course sections conflict with each other based on the constraint.
      *
-     * @param `section` the first course section
-     * @param `section1` the second course section
+     * @param section the first course section
+     * @param section1 the second course section
      * @return true if the two sections conflict, false otherwise
      */
     boolean twoConflictingCourses(Section section, Section section1);
@@ -41,8 +41,8 @@ public interface Constraint {
     /**
      * Generates a conflict between two course sections based on the constraint.
      *
-     * @param `section1` the first course section
-     * @param `section2` the second course section
+     * @param section1 the first course section
+     * @param section2 the second course section
      * @return an optional conflict between the two sections if they conflict, or empty if they don't
      */
     Optional<Conflict> generateConflict(Section section1, Section section2);
