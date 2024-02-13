cbda3d0dbaabca245799011a85f93887ac3dd302
thielmeiers <thielmeiers25@hanover.edu>
2024-02-13 13:53:57 -0500

    TwoCourseConflict javaDoc added

--- src/main/java/edu/hanover/schedulevisualizer/conflict/TwoCourseConflict.java
+++ src/main/java/edu/hanover/schedulevisualizer/conflict/TwoCourseConflict.java
@@ -4,21 +4,47 @@ import edu.hanover.schedulevisualizer.core.Section;
 
 import java.util.Objects;
 
+/**
+ * Represents a conflict between two course sections in a schedule.
+ * This class implements the Conflict interface and provides methods
+ * to check for equality, generate a hash code, and create a string
+ * representation of the conflict.
+ */
 public class TwoCourseConflict implements Conflict {
 
+    /** The first course section involved in the conflict. */
     Section section1;
+
+    /** The second course section involved in the conflict. */
     Section section2;
 
+    /**
+     * Constructs a TwoCourseConflict object with the specified course sections.
+     *
+     * @param section1 The first course section involved in the conflict.
+     * @param section2 The second course section involved in the conflict.
+     */
     public TwoCourseConflict(final Section section1, final Section section2) {
         this.section1 = section1;
         this.section2 = section2;
     }
 
+    /**
+     * Returns a string representation of the TwoCourseConflict object.
+     *
+     * @return The string representation of the TwoCourseConflict object.
+     */
     @Override
     public String toString() {
         return "TwoCourseConflict";
     }
 
+    /**
+     * Compares this TwoCourseConflict object with another object for equality.
+     *
+     * @param o The object to compare with.
+     * @return {@code true} if the objects are equal, {@code false} otherwise.
+     */
     @Override
     @SuppressWarnings("PMD.ShortVariable")
     public boolean equals(final Object o) {
@@ -28,6 +54,11 @@ public class TwoCourseConflict implements Conflict {
         return Objects.equals(section1, that.section1) && Objects.equals(section2, that.section2);
     }
 
+    /**
+     * Generates a hash code for the TwoCourseConflict object based on its content.
+     *
+     * @return The hash code for the TwoCourseConflict object.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(section1, section2);
