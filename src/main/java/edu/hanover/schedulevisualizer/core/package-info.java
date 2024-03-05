d23e4d58d6f801fa2ff49a9deebfe1c60f1b9c2f
jmckeigue <mckeiguej24@hanover.edu>
2024-03-05 13:03:48 -0500

    No issue made, simple commenting error (more than 80 characters)

--- src/main/java/edu/hanover/schedulevisualizer/core/package-info.java
+++ src/main/java/edu/hanover/schedulevisualizer/core/package-info.java
@@ -1,7 +1,7 @@
 /**
  * This is the **main package** for the schedule visualizer.
- * Look at {@link edu.hanover.schedulevisualizer.core.Context Context} for details.
+ * Look at {@link edu.hanover.schedulevisualizer.core.Context Context}
+ * for details.
  * @see edu.hanover.schedulevisualizer.core.Context Context
- *
  */
 package edu.hanover.schedulevisualizer.core;
