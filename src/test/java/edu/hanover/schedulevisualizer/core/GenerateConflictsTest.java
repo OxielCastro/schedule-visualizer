20783bdd236b2a3b572c470f6004ccee0d1a9ce7
Matt Burchett <burchettm26@hanover.edu>
2024-03-19 13:53:30 -0400

    Added a test in the GenerateConflictsTest class that shows the new InstructorConflict class works for the getConflicts method when passed a list of sections. #41

--- src/test/java/edu/hanover/schedulevisualizer/core/GenerateConflictsTest.java
+++ src/test/java/edu/hanover/schedulevisualizer/core/GenerateConflictsTest.java
@@ -1,6 +1,7 @@
 package edu.hanover.schedulevisualizer.core;
 
 import edu.hanover.schedulevisualizer.conflict.*;
+import edu.hanover.schedulevisualizer.core.entity.Instructor;
 import edu.hanover.schedulevisualizer.core.entity.Section;
 import edu.hanover.schedulevisualizer.core.entity.Weekday;
 import org.junit.jupiter.api.Test;
@@ -38,6 +39,26 @@ public class GenerateConflictsTest extends ContextAwareTest {
         List<Conflict> conflictlist = new ArrayList<>();
         Constraint demo = new TwoCourseTimeSlotConstraint(section1, section2);
         conflictlist.add(new TwoCourseTimeSlotConflict(section1, section3));
+        assertEquals(conflictlist, demo.getConflicts(sectionlist));
+    }
+
+    @Test
+    public void getConflictsTestForInstructorConstraintWithThreeCourses() {
+        Section section1 = ef.makeSection(ef.makeCourse("CS", "321", "Software Development Practicum"), ef.makeHCTimeSlot(Weekday.MWF(), 1));
+        Section section2 = ef.makeSection(ef.makeCourse("CS", "231", "System Fundamentals"), ef.makeHCTimeSlot(Weekday.MWF(), 1));
+        Section section3 = ef.makeSection(ef.makeCourse("CS", "220", "Fundamentals Of CS"), ef.makeHCTimeSlot(Weekday.MWF(), 3));
+        Instructor instructor1 = ef.makeInstructor("John", "Smith", "456357");
+        Instructor instructor2 = ef.makeInstructor("Colin", "Sharp", "456857");
+        section1.addInstructor(instructor1);
+        section2.addInstructor(instructor1);
+        section3.addInstructor(instructor2);
+        List<Section> sectionlist = new ArrayList<>();
+        sectionlist.add(section1);
+        sectionlist.add(section2);
+        sectionlist.add(section3);
+        List<Conflict> conflictlist = new ArrayList<>();
+        Constraint demo = new InstructorConstraint(section1, section2);
+        conflictlist.add(new InstructorConflict(section1, section2));
         assertEquals(conflictlist, demo.getConflicts(sectionlist));
     }
 }
